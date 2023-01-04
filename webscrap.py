8ba48ba231da21cb6b589ccbe9ee64b7177c1be7
Darsana-Dileep <darshana.potmain@gmail.com>
2023-01-04 20:55:06 +0530

    initial commit

---
+++ webscrap.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+wed_url = "https://xpayback.com/"
+
+res = requests.get(wed_url)
+soup =BeautifulSoup(res.text,"html5lib")
+
+jobs = soup.find_all("div",{"class":"banner-content"})
+#print(jobs)
+for job in jobs:
+    title_element = job.find("h1")
+    print(title_element.text)
+
+
+
+
+
+
